a82f07232fdca84ab41699203f2975b7531f6879
Alessia <ale.rainbow96@gmail.com>
2021-02-06 23:58:04 +0100

    Aggiunto: ColliderTrigger + relativi script per gestione Codex, Immagine freccia per rappresentare la posizione + puntino rosso con animazione. Modificato script "interactionManager" e "ShowAgenda" con le nuove funzionalità. Modificato i collider delle botti.

---
+++ Unity/Assets/Scripts/ObjectInteractable/CodexNewVoiceManager.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodexNewVoiceManager : MonoBehaviour
+{
+    public bool _newVoiceIsShowing = false;
+}
---
+++ Unity/Assets/Scripts/ObjectInteractable/InformationObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InformationObject : ObjectInteractable
+{
+    [SerializeField] private CodexInformation _objectName;
+    [SerializeField] private Text _nuovaVoceText;
+
+
+    public override void Interact()
+    {
+    }
+
+    public override void UITextOff()
+    {
+    }
+
+    public override void UITextOn()
+    {
+        StartCoroutine(NewVoice());
+        foreach(BoxCollider b in GetComponents<BoxCollider>())
+        {
+            b.enabled = false;
+        }
+    }
+
+    private IEnumerator NewVoice()
+    {
+        _nuovaVoceText.text = "Nuova nota nel codex: " + _objectName.ToString();
+        yield return new WaitForSeconds(5);
+        if(_nuovaVoceText.text == "Nuova nota nel codex: " + _objectName.ToString()) _nuovaVoceText.text = "";
+
+    }
+}
