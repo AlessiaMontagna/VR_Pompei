8ff0c380a392c34c2fdf5afd67a33cc610dd2c63
francescoalagna <s265350@studenti.polito.it>
2021-02-14 14:10:16 +0100

    Update NpcInteractable.cs

--- Unity/Assets/Scripts/Npc/NpcInteractable.cs
+++ Unity/Assets/Scripts/Npc/NpcInteractable.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 [RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
 [RequireComponent(typeof(Animator))]
-//[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(AudioSource))]
 [RequireComponent(typeof(OcclusionInteract))]
 
 public class NpcInteractable : Interattivo
@@ -19,8 +19,8 @@ public class NpcInteractable : Interattivo
     public Characters character => _character;
     private GameObject _parent;
     public GameObject parent => _parent;
-    //private AudioSource _audioSource;
-    //public AudioSource audioSource => _audioSource;
+    private AudioSource _audioSource;
+    public AudioSource audioSource => _audioSource;
     private OcclusionInteract _fmodAudioSource;
     public OcclusionInteract fmodAudioSource => _fmodAudioSource;
     private string _voice;
@@ -40,7 +40,7 @@ public class NpcInteractable : Interattivo
         _navAgent.SetInitialState(statename);
         _navAgent.SetTargets(targets);
         _animator = gameObject.GetComponent<Animator>();
-        //_audioSource = gameObject.GetComponent<AudioSource>();
+        _audioSource = gameObject.GetComponent<AudioSource>();
 
         _fmodAudioSource = gameObject.GetComponent<OcclusionInteract>();
         _fmodAudioSource.enabled = false;
@@ -101,9 +101,15 @@ public class NpcInteractable : Interattivo
 
     public void SetAudio(int index)
     {
+<<<<<<< HEAD
         _fmodAudioSource.enabled = (index >= 0);
         if(_fmodAudioSource.enabled){_fmodAudioSource.SelectAudio = "event:/"+ GameObject.FindObjectOfType<AudioSubManager>().GetAudio(index, _character, _voice);}
         //_audioSource.clip = Resources.Load<AudioClip>(GameObject.FindObjectOfType<AudioSubManager>().GetAudio(index, _character, _voice));
+=======
+        if(index < 0){_fmodAudioSource.enabled = false;return;}
+        _fmodAudioSource.SelectAudio = "event:/"+ GameObject.FindObjectOfType<AudioSubManager>().GetAudio(index, _character, _voice);
+        _audioSource.clip = Resources.Load<AudioClip>(GameObject.FindObjectOfType<AudioSubManager>().GetAudio(index, _character, _voice));
+>>>>>>> parent of b6ecb89 (Merge remote-tracking branch 'origin/Giorgio' into Fra)
         //if(_audioSource?.clip == null){Debug.LogError($"{GameObject.FindObjectOfType<AudioSubManager>().GetAudio(index, _character, _voice)} NOT FOUND");StopInteraction();}
         //_audioSource.Play();
     }
@@ -138,14 +144,14 @@ public class NpcInteractable : Interattivo
         _animator.SetBool(NavAgent.NavAgentStates.Talk.ToString(), true);
         SetAudio(index);
         SetSubtitles(index);
-        //yield return new WaitForSeconds(_audioSource.clip.length);
+        yield return new WaitForSeconds(_audioSource.clip.length);
 
-        int fmodLength;
+       /*  int fmodLength;
         float length = 0;
         FMOD.RESULT res = _fmodAudioSource.AudioDes.getLength(out fmodLength);
         length = fmodLength /1000;
-        //Debug.Log("waitForSecond " + length);
-        yield return new WaitForSeconds(length); 
+        Debug.Log("waitForSecond " + fmodLength);
+        yield return new WaitForSeconds(length); */
 
         StopInteraction();
     }
