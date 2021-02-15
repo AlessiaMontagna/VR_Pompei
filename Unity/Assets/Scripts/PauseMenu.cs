507c2e9c470593ea28884f5956ed4e684488f152
LAPTOP-ALTLTU7R\antom <anto.messina09@gmail.com>
2021-02-15 10:33:28 +0100

    aggiunto la UI

--- Unity/Assets/Scripts/PauseMenu.cs
+++ Unity/Assets/Scripts/PauseMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -10,6 +11,14 @@ public class PauseMenu : MonoBehaviour
 
     public GameObject pauseMenuUI;
     public FirstPersonController fps;
+    public GameObject optionsMenuHolder;
+    public GameObject controlsMenuHolder;
+
+    public Slider[] volumeSliders;
+    public Toggle[] resolutionToggles;
+    public int[] screenWidhts;
+    int activeScreenResIndex;
+
     public void Start()
     {
         fps = FindObjectOfType<FirstPersonController>();
@@ -32,6 +41,8 @@ public class PauseMenu : MonoBehaviour
    public void Resume()
     {
         pauseMenuUI.SetActive(false);
+        optionsMenuHolder.SetActive(false);
+        controlsMenuHolder.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
         fps.enabled = true;
@@ -50,10 +61,57 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 1f;
         SceneManager.LoadScene("LightsSetup 1");
     }
+    public void LoadPauseMenu()
+    {
+        pauseMenuUI.SetActive(true);
+        optionsMenuHolder.SetActive(false);
+        controlsMenuHolder.SetActive(false);
+    }
 
+    public void LoadOption()
+    {
+        pauseMenuUI.SetActive(false);
+        optionsMenuHolder.SetActive(true);
+        controlsMenuHolder.SetActive(false);
+    }
+    public void ControlsMenu()
+    {
+        pauseMenuUI.SetActive(false);
+        optionsMenuHolder.SetActive(false);
+        controlsMenuHolder.SetActive(true);
+    }
     public void QuitGame()
     {
-        Debug.Log("Quti...");
+        Debug.Log("Quit...");
         Application.Quit();
     }
+    public void SetScreenResolution(int i)
+    {
+        if (resolutionToggles[i].isOn)
+        {
+            activeScreenResIndex = i;
+            float aspectRatio = 16 / 9f;
+            Screen.SetResolution(screenWidhts[i], (int)(screenWidhts[i] / aspectRatio), false);
+        }
+    }
+    public void SetFullScreen(bool isFullScreen)
+    {
+        for (int i = 0; i < resolutionToggles.Length; i++)
+        {
+            resolutionToggles[i].interactable = !isFullScreen;
+
+        }
+
+        if (isFullScreen)
+        {
+            Resolution[] allResolutions = Screen.resolutions;
+            Resolution maxResolution = allResolutions[allResolutions.Length - 1];
+            Screen.SetResolution(maxResolution.width, maxResolution.height, true);
+        }
+        else
+        {
+            SetScreenResolution(activeScreenResIndex);
+        }
+    }
+
 }
