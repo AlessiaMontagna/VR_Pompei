824e32762738f1a1a9ce637840ca122f0be733d9
francescoalagna <s265350@studenti.polito.it>
2021-02-21 22:28:32 +0100

    Revert "li prendono ogni tanto"
    This reverts commit f01194ef799a7ff4266634659928f678b5c5d77f.

--- Unity/Assets/Scripts/ProjectileMoveScript.cs
+++ Unity/Assets/Scripts/ProjectileMoveScript.cs
@@ -5,64 +5,91 @@ using UnityEngine;
 public class ProjectileMoveScript : MonoBehaviour
 {
     public float Speed;
-    public GameObject impactPrefab;
-    public List<GameObject> trails;
+    public GameObject Impactprefab;
+    public List<GameObject> Trails;
     public SpawnLapillusScript parentScript;
     GameObject target;  
 
-    private Rigidbody _rb;
-    private bool _last;
+    private Rigidbody rb;    
+    private bool last;
+
+    public float GetSpeed()
+    {
+        return Speed;
+    }
 
     // Start is called before the first frame update
-    void Start(){_rb = GetComponent<Rigidbody>();}
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();        
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(Speed != 0 && _rb != null)
+        if(Speed != 0 && rb != null)
         {  
-            if(_last)_rb.position += target.transform.position * (Speed * Time.deltaTime);
-            else _rb.position += transform.forward * (Speed * Time.deltaTime);
+            if(last)
+            {
+                rb.position += target.transform.position * (Speed * Time.deltaTime);
+            } 
+            else 
+            {
+                rb.position += transform.forward * (Speed * Time.deltaTime);
+            }                                                                                          
         }
     }
 
     void OnCollisionEnter(Collision collision) 
     {
         Speed = 0;   
-        ContactPoint contact = collision.contacts[0];   //Quaternion.identity
-        Quaternion rot = Quaternion.FromToRotation(Vector3.up, Vector3.up); //, contact.point);
+        
+             
+
+        ContactPoint contact = collision.contacts[0]; //Quaternion.identity
+        Quaternion rot = Quaternion.FromToRotation(Vector3.up, Vector3.up);//, contact.point);
         Vector3 pos = contact.point;
         parentScript.TellHitPos(pos);                       
 
-        if(impactPrefab != null)
+        if(Impactprefab != null)
         {
-            var impactVFX = Instantiate(impactPrefab, pos, rot) as GameObject;
+            var impactVFX = Instantiate(Impactprefab, pos, rot) as GameObject;
+           
             //Check if lapil hit the floor
-            if(collision.gameObject.tag != "Floor")impactVFX.GetComponent<ImpactAudioScript>().StopParticles();
-            Destroy(impactVFX, 5);
+            if(collision.gameObject.tag != "Floor")
+            {            
+                impactVFX.GetComponent<ImpactAudioScript>().StopParticles();
+            }
+            Destroy (impactVFX, 5);
         }
 
-        if(trails.Count > 0)    //faccio durare la scia di fumo dopo che il lapillo ha colpito il terreno
+        if(Trails.Count > 0)    //faccio durare la scia di fumo dopo che il lapillo ha colpito il terreno
         {
-            for(int i = 0; i<trails.Count; i++)
+            for(int i = 0; i<Trails.Count; i++)
             {
-                trails[i].transform.parent = null;
-                var ps = trails[i].GetComponent<ParticleSystem>();
+                Trails[i].transform.parent = null;
+                var ps = Trails[i].GetComponent<ParticleSystem>();
                 if(ps!=null)
                 {
                     ps.Stop();
-                    Destroy(ps.gameObject, ps.main.duration + ps.main.startLifetime.constantMax);
+                    Destroy (ps.gameObject, ps.main.duration + ps.main.startLifetime.constantMax);
                 }
             }
         }
-        Destroy(gameObject);
+
+        Destroy (gameObject);
     } 
 
     public void IsLast(GameObject character)
     {
         target = character;
-        _last = true;
+        last = true;
     }
 
-    void OnTriggerEnter(Collider collider){if(collider?.tag == "NPC")collider.GetComponent<NpcInteractable>().OnExplosion(gameObject.transform.position);}
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider?.tag != "NPC")return;
+        Debug.Log("TRIGGERED");
+
+    }
 }
