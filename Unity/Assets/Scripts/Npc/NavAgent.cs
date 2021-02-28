e7097bd5cbba15c4fe15281d39c4f0b7e6d8426b
francescoalagna <s265350@studenti.polito.it>
2021-02-28 20:33:26 +0100

    il logo e l'ultima volta che vedo l'animator controller

--- Unity/Assets/Scripts/Npc/NavAgent.cs
+++ Unity/Assets/Scripts/Npc/NavAgent.cs
@@ -131,7 +131,7 @@ public class NavAgent
     {
         var player = GameObject.FindObjectOfType<InteractionManager>().gameObject.transform.position;
         if(Vector3.Distance(player, _owner.gameObject.transform.position) > maxInteractionDistance) _owner.Interaction(-1);
-        else if(Vector3.Distance(player, _owner.gameObject.transform.position) > 1.35f)TurnToTarget(player);
+        else if(Vector3.Distance(player, _owner.gameObject.transform.position) > 1.4f)TurnToTarget(player);
         else TurnToTarget(_owner.gameObject.transform.position + _owner.gameObject.transform.forward * 2f);
     }
 
